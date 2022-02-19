086f3fba8c288d71d01841a1c337abd3bb34efe1
anwarul1 <anwarjewel007@gmail.com>
2022-02-20 01:33:50 +0600

    4th commit

---
+++ let-const.js
@@ -0,0 +1,9 @@
+let numbers = ['abul', 'babu', 'sona', 'pona', 'anwar'];
+
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  //   console.log(number);
+  if (number.length % 2 != 0) {
+    element = number;
+  }
+}
