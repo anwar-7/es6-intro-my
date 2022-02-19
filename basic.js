13cd782b7104e87d86850804e22bdea5f9c57081
anwarul1 <anwarjewel007@gmail.com>
2022-02-19 22:13:20 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,40 @@
+// variable
+var deposit = 400;
+// condition
+if (deposit > 500) {
+  //
+} else if (deposit < 500) {
+  //
+} else if (deposit == 200) {
+  //
+} else if (deposit != 600) {
+  //
+} else if (deposit >= 300) {
+  //
+} else if (deposit <= 300) {
+  //
+} else {
+  //
+}
+// array
+const numbers = [45, 6587, 124, 45, 1, 365];
+const numberCount = numbers.length;
+// console.log(numberCount);
+numbers.push(121);
+numbers.pop();
+// number.pop();
+// number[2] = 'Anwar';
+// check whether 222 included in the array
+if (numbers.indexOf(222) != -1) {
+  // console.log(number);
+}
+if (numbers.includes(222)) {
+  // console.log(number);
+}
+
+// loop
+// while, for
+for (const number of numbers) {
+  // console.log(number);
+}
+// console.log(number);
