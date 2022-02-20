02bd478890683f2e8d4ff71ba5dacfff5db82c69
anwarul1 <anwarjewel007@gmail.com>
2022-02-20 17:39:01 +0600

    6th commit

---
+++ template-string.js
@@ -0,0 +1,12 @@
+const first = 'kotim';
+const second = 'ali';
+const oldSum = first + ' ' + second;
+// console.log(oldSum);
+const newSum = `${first} ${second}`;
+// console.log(newSum);
+const multiLine = 'this is a first line.' + 'this is a second line';
+const multiLineNew = `this is a first line. 
+this is a second line.
+this is a third line of ${first} ${second}.
+`;
+console.log(multiLineNew);
